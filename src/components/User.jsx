cf9c7e286c6b75c1b0557e6080def35428e1d61b
RaviTesla1 <ravibatalvi55@gmail.com>
2024-12-04 23:54:56 +0530

    state changed using setter method

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,14 +5,22 @@ import PropTypes from 'prop-types';
 class UserClass extends  React.Component{
     constructor(props){
         super(props);
-        
-        console.log("🚀 ~ UserClass ~ constructor ~ pr̥ops:", props)
+        this.state={
+            count:0
+        }
     }
  
     render(){
         const {name,teacher} = this.props;
+        const {count} = this.state;
 
         return <div>
+            <h2>Count : {count}</h2>
+            <button onClick={()=>{
+                this.setState({
+                    count:this.state.count + 1
+                })
+            }}>Increase Count</button>
             <h2>Name : {name}</h2>
             <h3>Teacher : {teacher}</h3>
             <h3>Location : Dehradun</h3>
